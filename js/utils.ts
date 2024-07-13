7ff01cd2d4abb968ddf8c60dd86d4d37fc59c792
Nikita Vinogradov <SamFisher@yandex.ru>
2024-07-14 00:53:37 +0300

    first commit

---
+++ js/utils.ts
@@ -0,0 +1,27 @@
+export class CommentaryUtils {
+	public createElementFromHTML(htmlString: string): HTMLElement {
+		let div = document.createElement("div");
+		div.innerHTML = htmlString.trim();
+
+		return <HTMLElement>div.firstChild;
+	}
+
+	public makeString(length: number) {
+		let result = "";
+		const characters =
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		const charactersLength = characters.length;
+
+		let counter = 0;
+		while (counter < length) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+			counter += 1;
+		}
+
+		return result;
+	}
+
+	public twoDigitDatePart(value: string | number) {
+		return ("0" + value).slice(-2);
+	}
+}
